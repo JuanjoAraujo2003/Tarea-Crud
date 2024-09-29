a3af8a2aa9ce3a3e367388f720decba046ee223c
Juan José Araujo <juan.araujo.bedon@udla.edu.ec>
2024-09-28 22:27:03 -0500

    Incorporación de operación actualizar y eliminar

--- warhammer_wiki/urls.py
+++ warhammer_wiki/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path, include
 from rest_framework import routers
 from .views import FactionView, index
+from .views import delete_faction, update_faction
 
 
 router = routers.DefaultRouter()
 router.register(r'factions', FactionView, 'factions')
 urlpatterns = [
     path("api/v1/", include(router.urls)),
-    path("", index , name="index")
+    path("", index , name="index"),
+    path("delete/<int:id>/", delete_faction, name="delete_faction"),
+    path("update/<int:id>/", update_faction, name="update_faction"),
 ]
--- warhammer_wiki/views.py
+++ warhammer_wiki/views.py
@@ -3,16 +3,38 @@ from .serializer import FactionSerializer
 from .models import Factions
 from rest_framework import viewsets
 from .forms import CreateNewFaction
+from django.shortcuts import get_object_or_404
+from django.urls import reverse
 
 class FactionView(viewsets.ModelViewSet):
     queryset = Factions.objects.all()
     serializer_class = FactionSerializer
 
 def index(request):
-    form = CreateNewFaction()
     if request.method == 'POST':
-        if "save" in request.POST:
             form = CreateNewFaction(request.POST)
-            form.save()
+            if form.is_valid():
+                form.save()
+                return redirect(reverse('index'))
+    else:
+        form = CreateNewFaction()
+                
     factions = Factions.objects.all()
     return render(request, 'index.html', {'form': form, 'factions': factions})
+
+def delete_faction(request, id):
+    faction = get_object_or_404(Factions, id=id)
+    if request.method == 'POST':
+        faction.delete()
+        return redirect('index')
+    
+def update_faction(request, id):
+    faction = get_object_or_404(Factions, id=id)
+    if request.method == 'POST':
+        form = CreateNewFaction(request.POST, instance=faction)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+    else:
+        form = CreateNewFaction(instance=faction)
+    return render(request, 'update.html', {'form': form})
