fd0d4c3e3d0fe834396933f79d93cedaa8a9c831
Juan José Araujo <juan.araujo.bedon@udla.edu.ec>
2024-10-05 23:28:08 -0500

    Agregación del Login y protección de URLs del CRUD

--- warhammer_wiki/urls.py
+++ warhammer_wiki/urls.py
@@ -2,13 +2,16 @@ from django.urls import path, include
 from rest_framework import routers
 from .views import FactionView, index
 from .views import delete_faction, update_faction
-
+from django.contrib.auth import views as auth_views
+from django.contrib.auth.decorators import login_required
 
 router = routers.DefaultRouter()
 router.register(r'factions', FactionView, 'factions')
 urlpatterns = [
     path("api/v1/", include(router.urls)),
-    path("", index , name="index"),
-    path("delete/<int:id>/", delete_faction, name="delete_faction"),
-    path("update/<int:id>/", update_faction, name="update_faction"),
+    path("", login_required(index), name="index"),
+    path("delete/<int:id>/", login_required(delete_faction), name="delete_faction"),
+    path("update/<int:id>/", login_required(update_faction), name="update_faction"),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
 ]
--- warhammer_wiki/views.py
+++ warhammer_wiki/views.py
@@ -5,31 +5,37 @@ from rest_framework import viewsets
 from .forms import CreateNewFaction
 from django.shortcuts import get_object_or_404
 from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+
 
 class FactionView(viewsets.ModelViewSet):
     queryset = Factions.objects.all()
     serializer_class = FactionSerializer
-
+@login_required
 def index(request):
-    if request.method == 'POST':
-        form = CreateNewFaction(request.POST)
-        if form.is_valid():
-            if "save" in request.POST:
+    if not request.user.is_authenticated:
+        return redirect('login')
+    else:
+        if request.method == 'POST':
+            form = CreateNewFaction(request.POST)
+            if form.is_valid():
                 form.save()
                 return redirect(reverse('index'))
-            
-    else:
-        form = CreateNewFaction()
                 
-    factions = Factions.objects.all()
-    return render(request, 'index.html', {'form': form, 'factions': factions})
-
+        else:
+            form = CreateNewFaction()
+                    
+        factions = Factions.objects.all()
+        return render(request, 'index.html', {'form': form, 'factions': factions})
+    
+@login_required
 def delete_faction(request, id):
     faction = get_object_or_404(Factions, id=id)
     if request.method == 'POST':
         faction.delete()
         return redirect('index')
     
+@login_required   
 def update_faction(request, id):
     faction = get_object_or_404(Factions, id=id)
     if request.method == 'POST':
